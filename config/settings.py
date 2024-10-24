fd405939a32baa469c9ea39b3cd587b1084c5842
Pakawin Mansukmanee <pakawinz@gmail.com>
2024-10-24 19:33:08 +0700

    config: add staticfiles dirs

--- config/settings.py
+++ config/settings.py
@@ -147,6 +147,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [
+    "./vue/dist/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

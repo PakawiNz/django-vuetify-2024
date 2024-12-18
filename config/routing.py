603763547ff5ed873a2c4009fa45a0b396e5e8d9
Pakawin Mansukmanee <pakawinz@gmail.com>
2024-12-18 09:16:34 +0700

    config: explicitly handle bad routing

--- config/routing.py
+++ config/routing.py
@@ -9,14 +9,13 @@ def route_apps(apps_settings):
     routes = []
 
     for app_name in apps_settings:
-        try:
-            module = import_module(f'apps.{app_name}.routes')
-        except ModuleNotFoundError:
+        if not (settings.BASE_DIR / 'apps' / app_name / 'routes.py').exists():
             continue
 
+        module = import_module(f'apps.{app_name}.routes')
         app_routes: list = getattr(module, 'routes', None)
         if not isinstance(app_routes, list):
-            continue
+            raise Exception(f'The "apps/{app_name}/routes.py" should have "routes" as a list.')
 
         for suffix, consumer in app_routes:
             routes.append(re_path(f'ws/{app_name}/{suffix}', consumer.as_asgi()))
